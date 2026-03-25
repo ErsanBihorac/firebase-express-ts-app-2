bd73dff82da96198ee46237edd91d61ee40b0e82
ersan <ersan@MacBook-Air-von-ersan.local>
2026-03-25 17:24:31 +0100

    project setup

---
+++ frontend/firebase-express-ts-app-2/src/app/app.config.ts
@@ -0,0 +1,20 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp } from 'firebase/app';
+import { environment } from '../environments/environment';
+import { provideAuth } from '@angular/fire/auth';
+import { getAuth } from 'firebase/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+  ],
+};
---
+++ frontend/firebase-express-ts-app-2/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAeFNi5BzRTM3qC9tzYM919g8AV8nEE65I',
+    authDomain: 'feta-assessment-2-7e46f.firebaseapp.com',
+    projectId: 'feta-assessment-2-7e46f',
+    storageBucket: 'feta-assessment-2-7e46f.firebasestorage.app',
+    messagingSenderId: '1071992814553',
+    appId: '1:1071992814553:web:04b271db5cf17f0e7037c8',
+  },
+};
